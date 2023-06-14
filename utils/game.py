c6323b8c4d99b4a8c399c3837f64baa21604d926
prngx <sochorm@outlook.com>
2023-06-14 15:35:05 +0200

    VScode started working :D after reinstall from official site

--- utils/game.py
+++ utils/game.py
@@ -1,12 +1,12 @@
 class Hangman:
     def __init__(self):
         possible_words=['becode', 'learning', 'mathematics', 'sessions']
-        word_to_find=["b","e","c","o","d","e"]
-        lives=5
-        correctly_guessed_letters=[]
-        wrongly_guessed_letters=[]
-        error_count=0
-        turn_count=0
+        self.word_to_find=["b","e","c","o","d","e"]
+        self.lives=5
+        self.correctly_guessed_letters=["_"] * len(self.word_to_find)
+        self.wrongly_guessed_letters=[]
+        self.error_count=0
+        self.turn_count=0
 
     def play(self):
         """ method that asks the player to enter a letter. 
@@ -17,30 +17,38 @@ class Hangman:
         guess = input("Give me an input letter.. ")
         while len(guess) > 1 or guess.isalpha() == False:
             guess = input("Must be letter and exactly just one letter!")
-        
-        for letter in self.word_to_find:
-            if letter==guess:
-                self.correctly_guessed_letters.append(guess)
-            else:
-                self.wrongly_guessed_letters.append(guess)
 
         
+        if guess in self.word_to_find:
+            self.correctly_guessed_letters.append(guess)
+        else:
+            self.wrongly_guessed_letters.append(guess)
+            self.error_count+=1
+            self.lives-=1  
 
-
-
+        
     def start_game(self):
         """ method that:
         will call play() until the game is over (because the user guessed the word or because of a game over).
         will call game_over() if lives is equal to 0.
         will call well_played() if all the letter are guessed.
         will print correctly_guessed_letters, bad_guessed_letters, life, error_count and turn_count at the end of each turn. """
+        
+        while self.lives > 0:
+            self.play()
+        else:
+            self.game_over()    
 
-    
+
     def game_over(self):
         # method that will stop the game and print game over....
         print("Game is over! You lost all your lives.")
-        quit()
+        ###  ????????  with pause ?????? quit()
 
     def well_played(self):
         # method that will print You found the word: {word_to_find_here} in {turn_count_here} turns with {error_count_here} errors!
         print(f"You found the word: {self.word_to_find} in {self.turn_count} turns with {self.error_count} errors!")
+
+
+game = Hangman()
+game.start_game()        
