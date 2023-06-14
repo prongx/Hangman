984ff1239a53419e07c8c8be31c81aed660a720a
prngx <sochorm@outlook.com>
2023-06-14 12:31:15 +0200

    lunch break commit :D

--- utils/game.py
+++ utils/game.py
@@ -13,7 +13,18 @@ class Hangman:
         Optional: Be careful that the player shouldn't be allowed to type something else than a letter, and not more than a letter. 
         If the player guessed a letter well, add it to the correctly_guessed_letters list. 
         If not, add it to the wrongly_guessed_letters list and add 1 to error_count."""
-        input("Give me an input letter")
+        
+        guess = input("Give me an input letter.. ")
+        while len(guess) > 1 or guess.isalpha() == False:
+            guess = input("Must be letter and exactly just one letter!")
+        
+        for letter in self.word_to_find:
+            if letter==guess:
+                self.correctly_guessed_letters.append(guess)
+            else:
+                self.wrongly_guessed_letters.append(guess)
+
+        
 
 
 
@@ -27,6 +38,9 @@ class Hangman:
     
     def game_over(self):
         # method that will stop the game and print game over....
+        print("Game is over! You lost all your lives.")
+        quit()
 
     def well_played(self):
         # method that will print You found the word: {word_to_find_here} in {turn_count_here} turns with {error_count_here} errors!
+        print(f"You found the word: {self.word_to_find} in {self.turn_count} turns with {self.error_count} errors!")
